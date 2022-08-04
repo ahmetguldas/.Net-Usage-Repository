edbc581e5a36e4f45d156d91f0e6ef1eccb9028d
ahmet.guldas <ahmet.guldas@hotmail.com>
2022-08-04 20:45:25 +0300

    Add project files.

---
+++ CORE_Usage_Repository/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CORE_Usage_Repository.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
